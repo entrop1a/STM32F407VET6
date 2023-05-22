7799141e885c6728fd491aa632a209c407c259c8
Eduardo <67566282+entrop1a@users.noreply.github.com>
2023-05-22 00:28:11 -0300

    Update uart.c

--- UART/uart.c
+++ UART/uart.c
@@ -21,7 +21,7 @@ void init_uart1(void){
 
 	// Pin configuration
 
-	RCC	-> APB2ENR |= RCC_APB2ENR_USART1EN;		//Enable clock to USART1
+	RCC -> APB2ENR |= RCC_APB2ENR_USART1EN;		//Enable clock to USART1
 	RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOAEN;		//Enable clock to GPIOA
 
 	GPIOA -> MODER |= GPIO_MODER_MODER9_1;  	//Set PA9 as OUTPUT Alternate function
@@ -31,25 +31,25 @@ void init_uart1(void){
 	GPIOA -> MODER &= ~GPIO_MODER_MODER10_0;
 
 	GPIOA -> AFR[1] |=   GPIO_AFRH_AFSEL9_0		//Set function of PA9 to USART1
-				    |    GPIO_AFRH_AFSEL9_1
-				    |    GPIO_AFRH_AFSEL9_2;
+			|    GPIO_AFRH_AFSEL9_1
+			|    GPIO_AFRH_AFSEL9_2;
 	GPIOA -> AFR[1] &= ~(GPIO_AFRH_AFSEL9_3);
 
 	GPIOA -> AFR[1] |=   GPIO_AFRH_AFSEL10_0	//Set function of PA10 to USART1
-				    |    GPIO_AFRH_AFSEL10_1
-				    |    GPIO_AFRH_AFSEL10_2;
+			|    GPIO_AFRH_AFSEL10_1
+			|    GPIO_AFRH_AFSEL10_2;
 	GPIOA -> AFR[1] &= ~(GPIO_AFRH_AFSEL10_3);
 
 
 	// Definitions of UART1
 
-	USART1 -> CR1 = 0x00; 			 //Reset state CR1
+	USART1 -> CR1 = 0x00; 	 	 	//Reset state CR1
 
-	USART1 -> CR1 |= USART_CR1_UE	 // Enable USART
-				  |  USART_CR1_TE	 // Enable TX
-				  |  USART_CR1_RE;	 // Enable RX
+	USART1 -> CR1 |= USART_CR1_UE	 	// Enable USART
+		      |  USART_CR1_TE	 	// Enable TX
+		      |  USART_CR1_RE;	 	// Enable RX
 
-	USART1 -> CR1 &= ~(USART_CR1_M); // Determine => [1 start bit, 8 data bits, n stop bit]
+	USART1 -> CR1 &= ~(USART_CR1_M);	// Determine => [1 start bit, 8 data bits, n stop bit]
 
 
 	// Boud rate
